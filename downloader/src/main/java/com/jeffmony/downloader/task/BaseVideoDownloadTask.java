2d2de32428e1a9196a2c917900b40af97a793863
JeffMony <jeffmony@163.com>
2020-04-09 01:22:29 +0800

    提交整视频下载代码

--- downloader/src/main/java/com/jeffmony/downloader/task/BaseVideoDownloadTask.java
+++ downloader/src/main/java/com/jeffmony/downloader/task/BaseVideoDownloadTask.java
@@ -3,10 +3,17 @@ package com.jeffmony.downloader.task;
 import com.jeffmony.downloader.VideoDownloadConfig;
 import com.jeffmony.downloader.listener.IDownloadTaskListener;
 import com.jeffmony.downloader.model.VideoDownloadInfo;
+import com.jeffmony.downloader.utils.LogUtils;
 
 import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class BaseVideoDownloadTask extends VideoDownloadTask {
+
+    private static final String TAG = "BaseVideoDownloadTask";
 
     private volatile long mCurLength = 0;
     private long mTotalLength = -1L;
@@ -25,7 +32,22 @@ public class BaseVideoDownloadTask extends VideoDownloadTask {
     }
 
     private void startDownload(long curLength) {
+        if (mDownloadInfo.getIsCompleted()) {
+            LogUtils.i(TAG, "BaseVideoDownloadTask local file.");
+            notifyDownloadProgress();
+            return;
+        }
+        startTimerTask();
+        mDownloadExecutor = new ThreadPoolExecutor(
+                THREAD_COUNT, THREAD_COUNT, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(), Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+        mDownloadExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
     }
 
     @Override
@@ -35,6 +57,10 @@ public class BaseVideoDownloadTask extends VideoDownloadTask {
 
     @Override
     public void resumeDownload() {
+
+    }
+
+    private void notifyDownloadProgress() {
 
     }
 }
